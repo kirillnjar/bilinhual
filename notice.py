966656e9b78500a08077a74133780553fa0d5798
Kirill <kirillnjar@users.noreply.github.com>
2020-03-07 19:14:58 +0300

    added keyboard

--- notice.py
+++ notice.py
@@ -1,7 +1,9 @@
+import json
+
 from apscheduler.schedulers.blocking import BlockingScheduler
-from sqlalchemy import func, types
+from sqlalchemy import func, types, and_
 from sqlalchemy.sql.functions import coalesce
-from viberbot.api.messages import TextMessage
+from viberbot.api.messages import TextMessage, KeyboardMessage
 
 from bot_database import *
 from main import viber
@@ -15,11 +17,12 @@ def notice_job():
     us = session.query(bot_users) \
         .outerjoin(bot_users.bot_users_answers) \
         .group_by(bot_users) \
-        .having(func.current_timestamp(type_=types.DateTime) - func.max(bot_users_answers.answer_date) > coalesce(
-        bot_users.notice_time, '00:30:00')) \
+        .having(and_(func.current_timestamp(type_=types.DateTime) - func.max(bot_users_answers.answer_date) > coalesce(
+        bot_users.notice_time, '00:30:00'), bot_users.notice_time != '00:00:00')) \
         .all()
     for u in us:
-        message = [TextMessage(text="Тест 1. Сообщение отправлено автоматически")]
+        message = [TextMessage(text="Тест 1. Сообщение отправлено автоматически"),
+                   KeyboardMessage(keyboard=json.load(open('start_keyboard.json', encoding='utf-8')))]
         viber.send_messages(u.viber_id, message)
 
 
