0a16058ee56afd1256d38c3090640f9462266902
Mtchl8 <37140509+Mtchl8@users.noreply.github.com>
2020-04-11 08:54:55 +0300

    s

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def incoming():
     return Response(status=200)
 
 
-if __name__ == "__main__":
 
-    app.run(host='localhost', port=8008, debug = True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
